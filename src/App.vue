66f1b74f43d903e06aaad795f29d939e1e2d225f
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-09-18 20:24:46 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Calculator from './components/Calculator.vue';
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-teal-500">
+      Hello world!
+    </h1>
+    <Calculator />
+  </div>
+</template>
